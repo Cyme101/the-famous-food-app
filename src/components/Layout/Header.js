36451b6f73a0d75fac801a6430a5b22b4003c271
Isabelle Vallerand <isabelle.vall@gmail.com>
2022-05-27 11:59:57 -0400

    Added the Cart Button with the css

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 
+import HeaderCartButton from './HeaderCartButton';
 import ketoMealsImage from '../../assets/keto-meals.png';
 import classes from './Header.module.css';
 
-const Header = (props) => {
+const Header = props => {
   return <Fragment>
     <header className={classes.header}>
       <h1>Keto Meals</h1>
-      <button>Cart</button>
+      <HeaderCartButton />
     </header>
     <div className={classes['main-image']}>
       <img src={ketoMealsImage} alt="Awesome keto meals" />
