fed97387259879684f83b3a9183f2fa195260291
Isabelle Vallerand <isabelle.vall@gmail.com>
2022-06-03 08:06:02 -0400

    Completed thefood app practice

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,16 +4,18 @@ import HeaderCartButton from './HeaderCartButton';
 import ketoMealsImage from '../../assets/keto-meals.png';
 import classes from './Header.module.css';
 
-const Header = props => {
-  return <Fragment>
-    <header className={classes.header}>
-      <h1>Keto Meals</h1>
-      <HeaderCartButton />
-    </header>
-    <div className={classes['main-image']}>
-      <img src={ketoMealsImage} alt="Awesome keto meals" />
-    </div>
-  </Fragment>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Keto Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={ketoMealsImage} alt="Awesome keto meals" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
